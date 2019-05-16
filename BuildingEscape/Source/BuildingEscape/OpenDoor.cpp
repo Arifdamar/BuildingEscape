eebe34eea0c39f362c96268a1a62af52b3701ca9
Arif Damar <arif660damar@outlook.com>
2019-05-16 15:46:09 +0300

    It's a new and clean start.

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	//..
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+
+void UOpenDoor::OpenDoor()
+{
+	// Find the owning Actor
+	AActor* Owner = GetOwner();
+
+	// Create a rotator
+	FRotator ActorRotation = Owner->GetActorRotation();
+
+	// Set the rotation value
+	ActorRotation.Yaw += 60;
+
+	// Set the door rotation
+	Owner->SetActorRotation(ActorRotation);
+	IsDoorOpen = true;
+}
+
+void UOpenDoor::CloseDoor()
+{
+	// Find the owning Actor
+	AActor* Owner = GetOwner();
+
+	// Create a rotator
+	FRotator ActorRotation = Owner->GetActorRotation();
+
+	// Set the rotation value
+	ActorRotation.Yaw -= 60;
+
+	// Set the door rotation
+	Owner->SetActorRotation(ActorRotation);
+	IsDoorOpen = false;
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Poll the Trigger Volume
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens) && !IsDoorOpen)
+	{
+		// When the ActorThatOpens is in the volume and the door is close
+		OpenDoor();
+	}
+	if (!PressurePlate->IsOverlappingActor(ActorThatOpens) && IsDoorOpen)
+	{
+		// When the ActorThatOpens is NOT in the volume and the door is open
+		CloseDoor();
+	}
+}
+
